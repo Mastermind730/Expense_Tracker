52a223f3873f233a6e058322ad0bc5c02d9a978a
Mastermind730 <sourav.97032@gmail.com>
2024-07-10 21:02:11 +0530

    dashboard half done

---
+++ src/app/(routes)/dashboard/_components/CardInfo.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { FullBudget } from '@/app/types';
+import { PiggyBank, Wallet } from 'lucide-react';
+import React, { useCallback, useEffect, useState } from 'react';
+
+interface CardInfoProps {
+  budget: FullBudget[];
+}
+
+const CardInfo: React.FC<CardInfoProps> = ({ budget }) => {
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [spendTotal, setTotalSpend] = useState(0);
+
+
+  const calculateCardInfo = useCallback(() => {
+    console.log(budget);
+    let total = 0;
+    let totalSpend = 0;
+    budget.forEach((item) => {
+      total += item?.amount || 0;
+      totalSpend += item?.totalSpend || 0;
+    });
+    console.log(total, totalSpend);
+    setTotalBudget(total);
+    setTotalSpend(totalSpend);
+  },[budget]);
+
+  useEffect(() => {
+    if (budget.length > 0) {
+      calculateCardInfo();
+    }
+  }, [budget,calculateCardInfo]);
+
+  
+
+  return (
+    <div>
+      {budget?.length>0 ? (
+        <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          <div className='p-7 border rounded-lg flex items-center justify-between'>
+            <div>
+              <h2 className='text-sm'>Total Budget</h2>
+              <h2 className='font-bold text-2xl'>${totalBudget}</h2>
+            </div>
+            <PiggyBank className='bg-primary p-3 h-12 w-12 rounded-full' />
+          </div>
+          <div className='p-7 border rounded-lg flex items-center justify-between'>
+            <div>
+              <h2 className='text-sm'>Total Spend</h2>
+              <h2 className='font-bold text-2xl'>${spendTotal}</h2>
+            </div>
+            <Wallet className='bg-primary p-3 h-12 w-12 rounded-full' />
+          </div>
+          <div className='p-7 border rounded-lg flex items-center justify-between'>
+            <div>
+              <h2 className='text-sm'>No. Of Budgets</h2>
+              <h2 className='font-bold text-2xl'>{budget.length}</h2>
+            </div>
+            <PiggyBank className='bg-primary p-3 h-12 w-12 rounded-full' />
+          </div>
+        </div>
+      ) : (
+        <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          {[1, 2, 3].map((item, index) => (
+            <div key={index} className='h-[160px] w-full bg-slate-200 animate-pulse rounded-lg'></div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CardInfo;
--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import { useUser } from '@clerk/nextjs'
+import React, { useEffect, useState } from 'react'
+import CardInfo from './_components/CardInfo';
+import axios from 'axios';
 
-const page = () => {
+const Page = () => {
+  const [budget,setBudget]=useState([]);
+    const fetchBudgets = async () => {
+        try {
+          const response = await axios.get('/api/getBudgets');
+          setBudget(response.data.budgets);
+        } catch (error) {
+          console.error('Error fetching budgets:', error);
+        }
+      };
+    
+      useEffect(() => {
+        fetchBudgets();
+      }, []);
+  const {user}=useUser();
   return (
-    <div>Dashboard</div>
+    <div className='p-2'>
+      <h2 className='font-bold text-3xl'>Hii,{user?.fullName} </h2>
+      <p className='text-gray-500'>Here&apos; what happening with your money. Lets manage your expenses</p>
+      <CardInfo budget={budget}/>
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
+          <div className='md:col-span-2'>
+Chart
+          </div>
+          <div className=''>
+Other content
+          </div>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default Page
